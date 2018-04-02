def602d2ac4bc7224eeb828c69a23578c0db9b57
taru.rata <saycheese5963@yahoo.co.jp>
2018-04-02 13:21:17 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=25)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField('タイトル', max_length=50)
+    text = models.TextField('本文')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+    thumbnail = models.ImageField(upload_to='images/', null='True', blank='True')
+
+    def __str__(self):
+        return self.title
+
+    def shortentext(self):
+        if len(self.text) > 20:
+            return self.text[:20] + '...'
+        else:
+            return self.text
+
+
+class Comment(models.Model):
+    name = models.CharField('お名前', max_length=30, default='名前')
+    text = models.TextField('本文')
+    post = models.ForeignKey(Post, verbose_name='紐づく記事', on_delete=models.PROTECT)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return self.text[:10]
---
+++ blog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.db.models import Q
+from django.views import generic
+from .models import Post, Category, Comment
+from .forms import CommentCreateForm
+
+
+# Create your views here.
+class IndexView(generic.ListView):
+    model = Post
+    paginate_by = 6
+
+    def get_queryset(self):
+        queryset = Post.objects.order_by('-created_at')
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(Q(title__icontains=keyword) | Q(text__contains=keyword))
+        return queryset
+
+
+class CategoryView(generic.ListView):
+    model = Post
+    paginate_by = 3
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        queryset = Post.objects.order_by('-created_at').filter(category=category)
+        return queryset
+
+
+class DetailView(generic.DetailView):
+    model = Post
+
+
+class CommentView(generic.CreateView):
+    model = Comment
+    form_class = CommentCreateForm
+
+    def form_valid(self, form):
+        post_pk = self.kwargs['post_pk']
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=post_pk)
+        comment.save()
+        return redirect('img:detail', pk=post_pk)
