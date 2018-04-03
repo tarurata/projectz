9d22489d6efa7b431fc8faa87369966b17777f37
taru.rata <saycheese5963@yahoo.co.jp>
2018-04-03 21:35:28 +0800

    css fix, add image upload func on admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import Category, Post, Image
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
 
-
+# class ImageInline(admin.StackedInline):
+#     model = Image
+#     extra = 3
+#
+#
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [ImageInline, ]
 class ImageInline(admin.TabularInline):
+    """記事内画像のインライン"""
     model = Image
     extra = 3
+
+
+class PostAdmin(admin.ModelAdmin):
+    """記事を、管理画面で画像をインラインで埋め込む"""
+    inlines = [ImageInline, ]
+
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Post, PostAdmin)  # インライン
--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
     thumbnail = models.ImageField('サムネイル', upload_to='images/', null='True', blank='True')
 
+
     def __str__(self):
         return self.title
 
@@ -42,5 +43,10 @@ class Image(models.Model):
     post = models.ForeignKey(
         Post, verbose_name='紐づく記事', on_delete=models.PROTECT
     )
-    src = models.ImageField('画像', upload_to='images/', null='True', blank='True')
+    src = models.ImageField('画像', upload_to='uploads/%Y/%m/%d/', blank=True, null=True)
     created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        """imgタグを生成する。これをそのまま記事欄に貼り付ける。"""
+        return "<img src='"+self.src.url+"'>"
+        # return '間接リンク:[filter imgpk]{0}[end] 直接リンク:[filter img]{1}[end]'.format(self.pk, self.src.url)
